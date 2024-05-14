778b7ce396790c34675b19a059b96efe9920573a
Sujeong Ji <suji.sujeongji@gmail.com>
2024-05-14 21:48:03 +0900

    03-useQuery-data-fetching-caching

---
+++ tanstack-query-v5/03-useQuery-data-fetching-caching/client/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import PostList1 from "./PostList1";
+import PostList2 from "./PostList2";
+import Post from "./Post";
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState(<PostList1 />);
+
+  return (
+    <div>
+      <button onClick={() => setCurrentPage(<PostList1 />)}>Post List 1</button>
+      <button onClick={() => setCurrentPage(<PostList2 />)}>Post List 2</button>
+      <button onClick={() => setCurrentPage(<Post id={1} />)}>
+        First Post
+      </button>
+      <br />
+      {currentPage}
+    </div>
+  );
+}
